266e64adda80d302be25fd6c8d8cbf2b6f291e61
David Cruz <david.cruz@unosquare.com>
2021-10-13 11:03:47 -0500

    Add new tables and migrations

--- leave_management/Data/ApplicationDbContext.cs
+++ leave_management/Data/ApplicationDbContext.cs
@@ -11,6 +11,13 @@ namespace leave_management.Data
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
         {
+
         }
+
+        public DbSet<Employee> Employees { get; set; }
+        public DbSet<LeaveAllocation> LeaveAllocations { get; set; }
+        public DbSet<LeaveRequest> LeaveRequests { get; set; }
+        public DbSet<LeaveType> LeaveTypes { get; set; }
+
     }
 }
