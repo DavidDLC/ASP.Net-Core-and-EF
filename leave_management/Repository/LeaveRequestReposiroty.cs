b873937383e937f3f43823c77b2308882418ae16
David Cruz <david.cruz@unosquare.com>
2021-10-13 11:32:29 -0500

    Setup Repository pattern and dependency injection

---
+++ leave_management/Repository/LeaveRequestReposiroty.cs
@@ -0,0 +1,48 @@
+﻿using leave_management.Contracts;
+using leave_management.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace leave_management.Repository
+{
+    public class LeaveRequestReposiroty : ILeaveRequestRepository
+    {
+        private readonly ApplicationDbContext _db;
+
+        public LeaveRequestReposiroty(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        public bool Create(LeaveRequest entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Delete(LeaveRequest entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ICollection<LeaveRequest> FindAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public LeaveRequest FindById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Save()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Update(LeaveRequest entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
